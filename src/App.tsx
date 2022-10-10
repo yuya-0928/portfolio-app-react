38641de0cb5a909be52f10e73c345b9d8c92ade2
yuya-0928 <y.hirako0928@gmail.com>
2022-10-10 21:55:21 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import yuni_profile_image from "./image/yuni_profile.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={yuni_profile_image}></img>
+      <p>ここはゆにがReactで遊ぶ場所だよ！</p>
+      <p>気が向いたら更新していくよ</p>
     </div>
   );
 }
