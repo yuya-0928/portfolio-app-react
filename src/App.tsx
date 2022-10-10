0583bc4bb3713f757202d57332a49cf833332661
yuya-0928 <y.hirako0928@gmail.com>
2022-10-10 22:00:28 +0900

    add alt props

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import yuni_profile_image from "./image/yuni_profile.jpg";
 function App() {
   return (
     <div className="App">
-      <img src={yuni_profile_image}></img>
+      <img src={yuni_profile_image} alt="This is profile img"></img>
       <p>ここはゆにがReactで遊ぶ場所だよ！</p>
       <p>気が向いたら更新していくよ</p>
     </div>
